b9898648c568310f2334fb6c829173a75cf0bbfa
Michael Steidl <namenodreamz@gmail.com>
2018-09-09 23:04:23 +0200

    Played with export and import statement

---
+++ math.js
@@ -0,0 +1,54 @@
+export class Vector2d {
+  constructor(x, y) {
+    this.height = height;
+    this.width = width;
+  }
+
+  add(another) {
+    return new Vector2d();
+  }
+
+  get x() {
+    return this.x;
+  }
+
+  /*
+   * Vector Addition with another Vector
+   * @return Returns another Vector
+   */
+  add(another) {
+    return new Vector2d(this.x + another.x, this.y + another.y);
+  }
+
+  mul(another) {}
+
+  length() {}
+
+  get toString() {
+    return `[${x},${y}]`;
+  }
+}
+
+module.exports = Vector2d;
+
+export class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  toString() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
+  getX() {
+    return this.x;
+  }
+  getY() {
+    return this.y;
+  }
+  move(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+module.exports = Point;
